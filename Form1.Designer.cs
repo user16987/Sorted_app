4334c361c0d2c67adbba917d9f63640cec2e84ec
user16789 <ilia.klimenko.work@yandex.ru>
2024-12-15 19:31:12 +1000

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Sorted_app
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxOriginal = new System.Windows.Forms.ListBox();
+            this.listBoxSorted = new System.Windows.Forms.ListBox();
+            this.textBoxMax = new System.Windows.Forms.TextBox();
+            this.textBoxMin = new System.Windows.Forms.TextBox();
+            this.textBoxSize = new System.Windows.Forms.TextBox();
+            this.textBoxResults = new System.Windows.Forms.TextBox();
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBoxOriginal
+            // 
+            this.listBoxOriginal.FormattingEnabled = true;
+            this.listBoxOriginal.ItemHeight = 16;
+            this.listBoxOriginal.Location = new System.Drawing.Point(264, 16);
+            this.listBoxOriginal.Name = "listBoxOriginal";
+            this.listBoxOriginal.Size = new System.Drawing.Size(99, 180);
+            this.listBoxOriginal.TabIndex = 0;
+            // 
+            // listBoxSorted
+            // 
+            this.listBoxSorted.FormattingEnabled = true;
+            this.listBoxSorted.ItemHeight = 16;
+            this.listBoxSorted.Location = new System.Drawing.Point(264, 242);
+            this.listBoxSorted.Name = "listBoxSorted";
+            this.listBoxSorted.Size = new System.Drawing.Size(99, 196);
+            this.listBoxSorted.TabIndex = 1;
+            // 
+            // textBoxMax
+            // 
+            this.textBoxMax.Location = new System.Drawing.Point(426, 151);
+            this.textBoxMax.Multiline = true;
+            this.textBoxMax.Name = "textBoxMax";
+            this.textBoxMax.Size = new System.Drawing.Size(49, 41);
+            this.textBoxMax.TabIndex = 2;
+            // 
+            // textBoxMin
+            // 
+            this.textBoxMin.Location = new System.Drawing.Point(426, 97);
+            this.textBoxMin.Multiline = true;
+            this.textBoxMin.Name = "textBoxMin";
+            this.textBoxMin.Size = new System.Drawing.Size(49, 41);
+            this.textBoxMin.TabIndex = 3;
+            // 
+            // textBoxSize
+            // 
+            this.textBoxSize.Location = new System.Drawing.Point(426, 48);
+            this.textBoxSize.Multiline = true;
+            this.textBoxSize.Name = "textBoxSize";
+            this.textBoxSize.Size = new System.Drawing.Size(49, 43);
+            this.textBoxSize.TabIndex = 4;
+            // 
+            // textBoxResults
+            // 
+            this.textBoxResults.Location = new System.Drawing.Point(385, 214);
+            this.textBoxResults.Multiline = true;
+            this.textBoxResults.Name = "textBoxResults";
+            this.textBoxResults.Size = new System.Drawing.Size(403, 224);
+            this.textBoxResults.TabIndex = 5;
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonGenerate.Location = new System.Drawing.Point(67, 48);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(174, 59);
+            this.buttonGenerate.TabIndex = 6;
+            this.buttonGenerate.Text = "Generate";
+            this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStart.Location = new System.Drawing.Point(64, 239);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(177, 59);
+            this.buttonStart.TabIndex = 7;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(481, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 29);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Size";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(481, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 29);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Min";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(481, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 29);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Max";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.buttonGenerate);
+            this.Controls.Add(this.textBoxResults);
+            this.Controls.Add(this.textBoxSize);
+            this.Controls.Add(this.textBoxMin);
+            this.Controls.Add(this.textBoxMax);
+            this.Controls.Add(this.listBoxSorted);
+            this.Controls.Add(this.listBoxOriginal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxOriginal;
+        private System.Windows.Forms.ListBox listBoxSorted;
+        private System.Windows.Forms.TextBox textBoxMax;
+        private System.Windows.Forms.TextBox textBoxMin;
+        private System.Windows.Forms.TextBox textBoxSize;
+        private System.Windows.Forms.TextBox textBoxResults;
+        private System.Windows.Forms.Button buttonGenerate;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sorted_app
+{
+    public partial class Form1 : Form
+    {
+        private int[] arrayToSort;
+        private int comparisonsBubble = 0;
+        private int swapsBubble = 0;
+        private long timeBubble = 0;
+        private int comparisonsSelection = 0;
+        private int swapsSelection = 0;
+        private long timeSelection = 0;
+        private int comparisonsInsertion = 0;
+        private int swapsInsertion = 0;
+        private long timeInsertion = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGenerate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int size = Convert.ToInt32(textBoxSize.Text);
+                int minValue = Convert.ToInt32(textBoxMin.Text);
+                int maxValue = Convert.ToInt32(textBoxMax.Text);
+                
+                if (size <= 0 || minValue > maxValue)
+                {
+                    MessageBox.Show("Некорректный ввод! Проверьте введённые данные.");
+                    return;
+                }
+
+                arrayToSort = new int[size];
+                Random random = new Random();
+
+                for (int i = 0; i < size; i++)
+                {
+                    arrayToSort[i] = random.Next(minValue, maxValue + 1);
+                }
+
+                listBoxOriginal.Items.Clear();
+                foreach (var item in arrayToSort)
+                {
+                    listBoxOriginal.Items.Add(item.ToString());
+                }
+
+                buttonStart.Enabled = true;
+
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show($"Ошибка ввода: {ex.Message}");
+            }
+            catch (OverflowException ex)
+            {
+                MessageBox.Show($"Ошибка ввода: {ex.Message}");
+            }
+        }
+
+        private void BubbleSort(int[] arr)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            int n = arr.Length;
+            bool swapped;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                swapped = false;
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    comparisonsBubble++;
+                    if (arr[j] > arr[j + 1])
+                    {
+                        Swap(ref arr[j], ref arr[j + 1]);
+                        swapped = true;
+                        swapsBubble++;
+                    }
+                }
+
+                if (!swapped)
+                {
+                    break;
+                }
+            }
+
+            stopwatch.Stop();
+            timeBubble = stopwatch.ElapsedMilliseconds;
+
+            Invoke(new Action(() =>
+            {
+                listBoxSorted.Items.AddRange(arr.Select(x => x.ToString()).ToArray());
+                MessageBox.Show("Пузырьковая сортировка завершена!");
+            }));
+        }
+
+        private void SelectionSort(int[] arr)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            int n = arr.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    comparisonsSelection++;
+                    if (arr[minIndex] > arr[j])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                if (minIndex != i)
+                {
+                    Swap(ref arr[i], ref arr[minIndex]);
+                    swapsSelection++;
+                }
+            }
+
+            stopwatch.Stop();
+            timeSelection = stopwatch.ElapsedMilliseconds;
+
+            Invoke(new Action(() =>
+            {
+                listBoxSorted.Items.AddRange(arr.Select(x => x.ToString()).ToArray());
+                MessageBox.Show("Сортировка выбором завершена!");
+            }));
+        }
+
+        private void InsertionSort(int[] arr)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            int n = arr.Length;
+            for (int i = 1; i < n; ++i)
+            {
+                int key = arr[i];
+                int j = i - 1;
+
+                while (j >= 0 && arr[j] > key)
+                {
+                    comparisonsInsertion++;
+                    arr[j + 1] = arr[j];
+                    j--;
+                    swapsInsertion++;
+                }
+
+                arr[j + 1] = key;
+            }
+
+            stopwatch.Stop();
+            timeInsertion = stopwatch.ElapsedMilliseconds;
+
+            Invoke(new Action(() =>
+            {
+                listBoxSorted.Items.AddRange(arr.Select(x => x.ToString()).ToArray());
+                MessageBox.Show("Сортировка вставками завершена!");
+            }));
+        }
+
+        private void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        private void DisplayResults()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("Результаты пузырьковой сортировки:");
+            sb.AppendLine($"   Сравнений: {comparisonsBubble}");
+            sb.AppendLine($"   Перестановок: {swapsBubble}");
+            sb.AppendLine($"   Время: {timeBubble} мс");
+
+            sb.AppendLine("\nРезультаты сортировки выбором:");
+            sb.AppendLine($"   Сравнений: {comparisonsSelection}");
+            sb.AppendLine($"   Перестановок: {swapsSelection}");
+            sb.AppendLine($"   Время: {timeSelection} мс");
+
+            sb.AppendLine("\nРезультаты сортировки вставками:");
+            sb.AppendLine($"   Сравнений: {comparisonsInsertion}");
+            sb.AppendLine($"   Перестановок: {swapsInsertion}");
+            sb.AppendLine($"   Время: {timeInsertion} мс");
+
+            textBoxResults.Text = sb.ToString();
+        }
+
+        private async void buttonStart_Click(object sender, EventArgs e)
+        {
+            
+            listBoxSorted.Items.Clear();
+            textBoxResults.Clear();
+
+            
+            Task taskBubble = Task.Run(() => BubbleSort(arrayToSort));
+            Task taskSelection = Task.Run(() => SelectionSort(arrayToSort));
+            Task taskInsertion = Task.Run(() => InsertionSort(arrayToSort));
+
+            await Task.WhenAll(taskBubble, taskSelection, taskInsertion);
+
+            DisplayResults();
+        }
+    }
+}
